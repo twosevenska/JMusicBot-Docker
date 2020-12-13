055c7119ecb0e8a41bc71025ad96b9e08838d20c
Goncalo Barroso <twosevenska@gmail.com>
2020-12-13 23:47:45 +0000

    Use shell script instead in the circleci config

--- DockerBuildPush.sh
+++ DockerBuildPush.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -x
+set -o nounset # Exit script if you try to use an uninitialized variable
+set -o errexit # Exit script if a statement returns a non-true return value
+set -o pipefail # Use the error status of the first failure, rather than that of the last item in a pipeline
 
 ./fetchBin.sh
 docker build . -t twosevenska/jmusicbot:latest
