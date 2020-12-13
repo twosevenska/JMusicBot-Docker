0a37984d5ce5d179927b2bdd3d134b70f4efccdb
Goncalo Barroso <twosevenska@gmail.com>
2020-12-13 23:55:49 +0000

    Fix shellcheck linter errors

--- DockerBuildPush.sh
+++ DockerBuildPush.sh
@@ -12,7 +12,7 @@ docker push twosevenska/jmusicbot:latest
 ver=$(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest \
     | grep tag_name \
 	| cut -d '"' -f 4)
-docker tag twosevenska/jmusicbot:latest twosevenska/jmusicbot:$ver
-docker push twosevenska/jmusicbot:$ver
+docker tag twosevenska/jmusicbot:latest "twosevenska/jmusicbot:$ver"
+docker push "twosevenska/jmusicbot:$ver"
 
 rm build/*
