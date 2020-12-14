8bd2cdd70db0b3ed85bd92ea7bc0eb75886b163b
Goncalo Barroso <twosevenska@gmail.com>
2020-12-14 00:08:36 +0000

    Add contingency for when build dir does not exist

--- fetchBin.sh
+++ fetchBin.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -x
+set -o nounset # Exit script if you try to use an uninitialized variable
+set -o errexit # Exit script if a statement returns a non-true return value
+set -o pipefail # Use the error status of the first failure, rather than that of the last item in a pipeline
+
+if [ ! -d build ]; then
+  mkdir build
+fi
+
 curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest \
     | grep browser_download_url \
 	| cut -d '"' -f 4 \
